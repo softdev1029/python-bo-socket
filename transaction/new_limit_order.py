8917e6f40088b9e3054aa0b8453a6d80ee3edb21
krischou <krisdevchou@gmail.com>
2021-07-01 05:53:10 +0300

    add New Limit Order

---
+++ transaction/new_limit_order.py
@@ -0,0 +1,65 @@
+import struct
+from utils.helper import print_bytes_hex
+from base.message import Message
+
+
+class NewLimitOrder(Message):
+    def __init__(self):
+        self.data = ()
+        self.binary_data = None
+
+    def set_data(
+        self,
+        data1,
+        data2,
+        data3,
+        messageType,
+        padding,
+        account,
+        orderID,
+    ):
+        self.data = (
+            data1.encode("utf-8"),
+            data2.encode("utf-8"),
+            data3,
+            messageType,
+            padding,
+            account,
+            orderID,
+
+
+
+            
+        )
+
+    def encode_binary_string(self):
+        try:
+            s = struct.Struct(
+                "= 1s 1s H H H I Q"
+            )
+            self.binary_data = s.pack(*(self.data))
+            print_bytes_hex("Encoded Risk Update Request message", self.binary_data, "")
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def decode_binary_string(self, data):
+        try:
+            s = struct.Struct(
+                "= 1s 1s H H H I H d d d d d 1s d d d d d d d d d d d I I I"
+            )
+            unpacked_data = s.unpack(data)
+            print("Decoded Risk Update Request message", unpacked_data)
+            self.binary_data = data
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def parse_header(self, data):
+        return True
+
+    def parse_message(self, data):
+        self.encode_binary_string()
+        return True
