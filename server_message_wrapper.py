6963217fe281200046e17d7420599ba449d73162
krischou <krisdevchou@gmail.com>
2021-07-08 21:14:23 +0300

    in server, interval 3 seconds

--- server_message_wrapper.py
+++ server_message_wrapper.py
@@ -1,5 +1,6 @@
 from auth.client_logon import ClientLogon
 from base.message_wrapper import MessageWrapper
+import time
 
 
 class ServerMessageWrapper(MessageWrapper):
@@ -11,31 +12,34 @@ class ServerMessageWrapper(MessageWrapper):
     def read(self):
         super(ServerMessageWrapper, self).read()
 
-        message = ClientLogon()
-        message.set_data(
-            "H",  # data1
-            "",  # data2
-            143,  # data3
-            1,  # LogonType
-            100700,  # Account
-            "1F6A",  # 2FA
-            "BOU7",  # UserName
-            506,  # TradingSessionID
-            "1",  # PrimaryOESIP
-            "1",  # SecondaryOESIP
-            "1",  # PrimaryMDIP
-            "1",  # SecondaryIP
-            0,  # SendingTime
-            1500201,  # MsgSeqNum
-            432451,  # Key
-            0,  # LoginStatus
-            0,  # RejectReason
-            "",  # RiskMaster
-        )
-        self.msgObj.encode()
-        self.msgObj = message
         self._set_selector_events_mask("w")
 
     def write(self):
-        self._write()
+        for i in range(2):
+            time.sleep(3)
+            message = ClientLogon()
+            message.set_data(
+                "H",  # data1
+                "",  # data2
+                143,  # data3
+                1,  # LogonType
+                100700,  # Account
+                "1F6A",  # 2FA
+                "BOU7",  # UserName
+                506,  # TradingSessionID
+                "1",  # PrimaryOESIP
+                "1",  # SecondaryOESIP
+                "1",  # PrimaryMDIP
+                "1",  # SecondaryIP
+                0,  # SendingTime
+                1500201,  # MsgSeqNum
+                432451,  # Key
+                0,  # LoginStatus
+                0,  # RejectReason
+                "",  # RiskMaster
+            )
+            self.msgObj.encode()
+            self.msgObj = message
+            self._write()
+
         self._set_selector_events_mask("r")
