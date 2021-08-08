283c11d233d6d2d434735606793c09cd5404041a
krischou <krisdevchou@gmail.com>
2021-08-08 21:19:22 +0300

    refactor TenLevelMessage with Base level message

---
+++ market/base_level_data.py
@@ -0,0 +1,51 @@
+import struct
+from utils.helper import print_bytes_hex
+from base.message import Message
+from base.logger import log
+
+
+class BaseLevelData(Message):
+    def __init__(self):
+        self.data = ()
+        self.binary_data = None
+        self.level_count = 20  # Ten Level Data message
+
+    def set_data(self, *data):
+        self.data = [d if not isinstance(d, str) else d.encode("utf-8") for d in data]
+
+    def set_extend_data(self, *data):
+        self.data.extend(
+            [d if not isinstance(d, str) else d.encode("utf-8") for d in data]
+        )
+
+    def encode(self):
+        try:
+            format = "= 1s 1s H H H H H q I H 12s"
+            for i in range(self.level_count):
+                format += " d d B"
+            s = struct.Struct(format)
+            self.binary_data = s.pack(*(self.data))
+            log(self.level_count / 2, "Level ...")
+            print_bytes_hex("Encoded Level Data message", self.binary_data, "")
+            return True
+        except Exception as e:
+            log(e)
+            return False
+
+    def decode(self, data):
+        try:
+            format = "= 1s 1s H H H H H q I H 12s"
+            for i in range(self.level_count):
+                format += " d d B"
+            s = struct.Struct(format)
+            unpacked_data = s.unpack(data)
+            log(self.level_count / 2, "Level ...")
+            log("Decoded Level Data message", unpacked_data)
+            self.binary_data = data
+            return True
+        except Exception as e:
+            log(e)
+            return False
+
+    def parse_message(self, data):
+        return self.decode(data)
--- market/ten_level_data.py
+++ market/ten_level_data.py
@@ -1,51 +1,14 @@
 import struct
 from utils.helper import print_bytes_hex
-from base.message import Message
+from market.base_level_data import BaseLevelData
 from base.logger import log
 
 
-class TenLevelData(Message):
+class TenLevelData(BaseLevelData):
     def __init__(self):
         self.data = ()
         self.binary_data = None
-
-    def set_data(self, *data):
-        self.data = [d if not isinstance(d, str) else d.encode("utf-8") for d in data]
-
-    def set_extend_data(self, *data):
-        self.data.extend(
-            [d if not isinstance(d, str) else d.encode("utf-8") for d in data]
-        )
-
-    def encode(self):
-        try:
-            format = "= 1s 1s H H H H H q I H 12s"
-            for i in range(20):
-                format += " d d B"
-            s = struct.Struct(format)
-            self.binary_data = s.pack(*(self.data))
-            print_bytes_hex("Encoded Ten Level Data message", self.binary_data, "")
-            return True
-        except Exception as e:
-            log(e)
-            return False
-
-    def decode(self, data):
-        try:
-            format = "= 1s 1s H H H H H q I H 12s"
-            for i in range(20):
-                format += " d d B"
-            s = struct.Struct(format)
-            unpacked_data = s.unpack(data)
-            log("Decoded Ten Level Data message", unpacked_data)
-            self.binary_data = data
-            return True
-        except Exception as e:
-            log(e)
-            return False
-
-    def parse_message(self, data):
-        return self.decode(data)
+        self.level_count = 20  # Ten Level Data message
 
 
 def create_ten_level_data():
