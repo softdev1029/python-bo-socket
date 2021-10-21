3ad7f83f3ca8aa2b6958e133f943d57f9951db82
krischou <krisdevchou@gmail.com>
2021-10-20 23:42:15 -0400

    add logConfig

--- base/logger.py
+++ base/logger.py
@@ -1,19 +1,35 @@
 import logging
 import time
 
-IS_LOGGING=True
-IS_FILE=False
+IS_LOGGING = True
+IS_FILE = False
 
 if IS_FILE:
-    LOG_FILENAME = 'logging_example.out' + str(time.time())
+    LOG_FILENAME = "logging_example.out" + str(time.time())
     logging.basicConfig(
         filename=LOG_FILENAME,
         level=logging.DEBUG,
     )
 
+
 def convertTuple(tup):
-    st = ' '.join(map(str, tup))
+    st = " ".join(map(str, tup))
     return st
+
+
+def logConfig(
+    isLogging=True, isFile=False, filename="logging_example.out", level=logging.DEBUG
+):
+    global IS_LOGGING
+    global IS_FILE
+    IS_LOGGING = isLogging
+    IS_FILE = isFile
+    if IS_FILE:
+        logging.basicConfig(
+            filename=filename,
+            level=level,
+        )
+
 
 def log(*args):
     if IS_LOGGING:
